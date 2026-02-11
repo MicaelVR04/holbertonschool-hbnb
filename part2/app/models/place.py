fa798cf86d5f115ba41afe0dbc6b51c7848ab3d4
Micael Velez <mvrod2314@gmail.com>
2026-02-11 06:17:48 +0000

    plc

--- part2/app/models/place.py
+++ part2/app/models/place.py
@@ -3,6 +3,17 @@ from app.models.base_model import BaseModel
 class Place(BaseModel):
     def __init__(self, title, description, price, latitude, longitude, owner):
         super().__init__()
+        
+        # Validation
+        if not title or not description:
+            raise ValueError("Title and description are required")
+        if price <= 0:
+            raise ValueError("Price must be greater than 0")
+        if not (-90 <= latitude <= 90):
+            raise ValueError("Latitude must be between -90 and 90")
+        if not (-180 <= longitude <= 180):
+            raise ValueError("Longitude must be between -180 and 180")
+        
         self.title = title
         self.description = description
         self.price = price
