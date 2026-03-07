831ed6c03d21f302caa2be3eef0dcae8b40817ab
Micael Velez <mvrod2314@gmail.com>
2026-03-07 04:10:50 +0000

    base

--- part3/app/models/base_model.py
+++ part3/app/models/base_model.py
@@ -5,21 +5,17 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class BaseModel(db.Model):
-    """Base class for all models in the HBnB project."""
     __abstract__ = True
 
     id = db.Column(db.String(60), primary_key=True, default=lambda: str(uuid.uuid4()))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
 
-    def save(self):
-        """Save the object to the database."""
-        db.session.add(self)
-        db.session.commit()
-
-    def update(self, data):
-        """Update attributes and save."""
-        for key, value in data.items():
-            if hasattr(self, key):
-                setattr(self, key, value)
-        self.save()
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not self.id:
+            self.id = str(uuid.uuid4())
+        if not self.created_at:
+            self.created_at = datetime.utcnow()
+        if not self.updated_at:
+            self.updated_at = datetime.utcnow()
