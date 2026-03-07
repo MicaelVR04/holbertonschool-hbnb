2fdb45cf764a72ba11359dbbdea10e74f13ef3cc
Micael Velez <mvrod2314@gmail.com>
2026-03-07 01:18:58 +0000

    config

--- part3/config.py
+++ part3/config.py
@@ -4,11 +4,20 @@ class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-123'
     DEBUG = False
 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///development.db"
+    )
+
+
 class DevelopmentConfig(Config):
     DEBUG = True
 
+
 class ProductionConfig(Config):
     DEBUG = False
+
 
 config = {
     'development': DevelopmentConfig,
