849926a6086999cb42d7c869057b559edadb9f8d
Micael Velez <mvrod2314@gmail.com>
2026-03-07 01:19:03 +0000

    run

--- part3/run.py
+++ part3/run.py
@@ -1,7 +1,9 @@
 from flask import Flask
 from app.api import blueprint
+from config import config
 
 app = Flask(__name__)
+app.config.from_object(config['default'])
 
 # Register the API blueprint
 app.register_blueprint(blueprint)
@@ -11,4 +13,4 @@ def hello():
     return "HBnB Project is Running!"
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=app.config['DEBUG'])
