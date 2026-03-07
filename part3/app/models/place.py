da7e7e8cd14992927efe23f25a34511ac9298a28
Micael Velez <mvrod2314@gmail.com>
2026-03-07 01:19:15 +0000

    plc

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,33 +1,16 @@
-from app.models.base_model import BaseModel
+from app.models.base_model import BaseModel, db
 
 class Place(BaseModel):
-    def __init__(self, title, description, price, latitude, longitude, owner):
-        super().__init__()
-        
-        # Validation
-        if not title or not description:
-            raise ValueError("Title and description are required")
-        if price <= 0:
-            raise ValueError("Price must be greater than 0")
-        if not (-90 <= latitude <= 90):
-            raise ValueError("Latitude must be between -90 and 90")
-        if not (-180 <= longitude <= 180):
-            raise ValueError("Longitude must be between -180 and 180")
-        
-        self.title = title
-        self.description = description
-        self.price = price
-        self.latitude = latitude
-        self.longitude = longitude
-        self.owner = owner  # This should be a User object
-        self.amenities = []  # List of Amenity objects
-        self.reviews = []    # List of Review objects
+    """Place model."""
 
-    def add_amenity(self, amenity):
-        """Associate an amenity with this place."""
-        if amenity not in self.amenities:
-            self.amenities.append(amenity)
+    __tablename__ = "places"
 
-    def add_review(self, review):
-        """Add a review to this place."""
-        self.reviews.append(review)
+    title = db.Column(db.String(128), nullable=False)
+    description = db.Column(db.String(1024), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    latitude = db.Column(db.Float, nullable=False)
+    longitude = db.Column(db.Float, nullable=False)
+
+    owner_id = db.Column(db.String(60), db.ForeignKey("users.id"), nullable=False)
+
+    reviews = db.relationship("Review", backref="place", lazy=True)
