12e38c9e7c0b9e000020376033863bf25c7f35d0
Micael Velez <mvrod2314@gmail.com>
2026-03-07 01:44:12 +0000

    init

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,2 +1,12 @@
-# HBnB Application Package
-# This file initializes the Flask app and its configurations
+from flask import Flask
+from app.api import blueprint
+
+
+def create_app(config_class="config.DevelopmentConfig"):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Register API blueprint
+    app.register_blueprint(blueprint)
+
+    return app
