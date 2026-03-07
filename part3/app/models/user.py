8e438850361d36ab42c2e24aa8abe425ea300692
Micael Velez <mvrod2314@gmail.com>
2026-03-07 04:10:55 +0000

    usr

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,4 +1,6 @@
 from app.models.base_model import BaseModel, db
+from app import bcrypt
+
 
 class User(BaseModel):
     """User model."""
@@ -8,8 +10,17 @@ class User(BaseModel):
     first_name = db.Column(db.String(128), nullable=False)
     last_name = db.Column(db.String(128), nullable=False)
     email = db.Column(db.String(128), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     is_admin = db.Column(db.Boolean, default=False)
 
     # Relationships
     places = db.relationship("Place", backref="owner", lazy=True)
     reviews = db.relationship("Review", backref="author", lazy=True)
+
+    def hash_password(self, password):
+        """Hashes the password before storing it."""
+        self.password = bcrypt.generate_password_hash(password).decode("utf-8")
+
+    def verify_password(self, password):
+        """Verifies if provided password matches hashed password."""
+        return bcrypt.check_password_hash(self.password, password)
