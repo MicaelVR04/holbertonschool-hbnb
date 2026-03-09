87313478c979450a69483bff1903f41453a28bec
Micael Velez <mvrod2314@gmail.com>
2026-03-09 14:54:45 +0000

    init

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,14 +1,18 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
+from flask_jwt_extended import JWTManager
 
 bcrypt = Bcrypt()
+jwt = JWTManager()
 
 
 def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
     app.config.from_object(config_class)
+    app.config["JWT_SECRET_KEY"] = app.config["SECRET_KEY"]
 
     bcrypt.init_app(app)
+    jwt.init_app(app)
 
     from app.api import blueprint
     app.register_blueprint(blueprint)
--- part3/app/api/__init__.py
+++ part3/app/api/__init__.py
@@ -4,19 +4,20 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.places import api as places_ns
 from app.api.v1.reviews import api as reviews_ns
+from app.api.v1.auth import auth_api, protected_api
 
-# Create a blueprint for the API
 blueprint = Blueprint('api', __name__, url_prefix='/api/v1')
 
-# Create the Api object
-api = Api(blueprint,
+api = Api(
+    blueprint,
     title='HBnB API',
     version='1.0',
     description='The HBnB Application API'
 )
 
-# Register all namespaces (endpoint groups)
 api.add_namespace(users_ns, path='/users')
 api.add_namespace(amenities_ns, path='/amenities')
 api.add_namespace(places_ns, path='/places')
 api.add_namespace(reviews_ns, path='/reviews')
+api.add_namespace(auth_api, path='/auth')
+api.add_namespace(protected_api, path='')
