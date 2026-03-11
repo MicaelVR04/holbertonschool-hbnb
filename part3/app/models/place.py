a6e8243c4e36f35bd6b10063f883505d679dc8f8
Micael Velez <mvrod2314@gmail.com>
2026-03-11 23:55:16 +0000

    place

--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,16 +1,20 @@
 from app.models.base_model import BaseModel, db
 
-class Place(BaseModel):
-    """Place model."""
 
+class Place(BaseModel):
     __tablename__ = "places"
 
     title = db.Column(db.String(128), nullable=False)
-    description = db.Column(db.String(1024), nullable=False)
+    description = db.Column(db.Text, nullable=False)
     price = db.Column(db.Float, nullable=False)
     latitude = db.Column(db.Float, nullable=False)
     longitude = db.Column(db.Float, nullable=False)
+    owner_id = db.Column(db.String(60), nullable=False)
 
-    owner_id = db.Column(db.String(60), db.ForeignKey("users.id"), nullable=False)
+    @property
+    def reviews(self):
+        return []
 
-    reviews = db.relationship("Review", backref="place", lazy=True)
+    @property
+    def amenities(self):
+        return []
