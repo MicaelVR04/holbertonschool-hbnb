c08c4213af4d5b5e2685c85828420beefda3b774
Micael Velez <mvrod2314@gmail.com>
2026-03-11 23:55:20 +0000

    rev

--- part3/app/models/review.py
+++ part3/app/models/review.py
@@ -1,12 +1,10 @@
 from app.models.base_model import BaseModel, db
 
-class Review(BaseModel):
-    """Review model."""
 
+class Review(BaseModel):
     __tablename__ = "reviews"
 
-    text = db.Column(db.String(1024), nullable=False)
+    text = db.Column(db.Text, nullable=False)
     rating = db.Column(db.Integer, nullable=False)
-
-    user_id = db.Column(db.String(60), db.ForeignKey("users.id"), nullable=False)
-    place_id = db.Column(db.String(60), db.ForeignKey("places.id"), nullable=False)
+    place_id = db.Column(db.String(60), nullable=False)
+    user_id = db.Column(db.String(60), nullable=False)
