c355364d475985a42d4bf8b81ca281f7ed680796
Micael Velez <mvrod2314@gmail.com>
2026-03-11 23:55:23 +0000

    usr

--- part3/app/models/user.py
+++ part3/app/models/user.py
@@ -1,10 +1,9 @@
+
 from flask_bcrypt import generate_password_hash, check_password_hash
 from app.models.base_model import BaseModel, db
 
 
 class User(BaseModel):
-    """User model."""
-
     __tablename__ = "users"
 
     first_name = db.Column(db.String(128), nullable=False)
@@ -13,14 +12,8 @@ class User(BaseModel):
     password = db.Column(db.String(255), nullable=False)
     is_admin = db.Column(db.Boolean, default=False, nullable=False)
 
-    # Relationships
-    places = db.relationship("Place", backref="owner", lazy=True)
-    reviews = db.relationship("Review", backref="author", lazy=True)
-
     def hash_password(self, password):
-        """Hashes the password before storing it."""
         self.password = generate_password_hash(password).decode("utf-8")
 
     def verify_password(self, password):
-        """Verifies if provided password matches hashed password."""
         return check_password_hash(self.password, password)
