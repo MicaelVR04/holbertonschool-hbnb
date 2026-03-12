5c60599a4c2ab56b880fce7a00e518fc6db630b4
Micael Velez <mvrod2314@gmail.com>
2026-03-12 00:40:26 +0000

    amen

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,7 +1,16 @@
 from app.models.base_model import BaseModel, db
+from app.models.place import place_amenity
 
 
 class Amenity(BaseModel):
     __tablename__ = "amenities"
 
     name = db.Column(db.String(128), nullable=False, unique=True)
+
+    # Amenity <-> Place (many-to-many)
+    places = db.relationship(
+        "Place",
+        secondary=place_amenity,
+        back_populates="amenities",
+        lazy="subquery",
+    )
