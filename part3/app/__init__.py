1161cea1ce46543f37516e29558bda8b9f79d797
Micael Velez <mvrod2314@gmail.com>
2026-03-31 01:24:11 +0000

    init

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager
+from flask_cors import CORS
 from app.models.base_model import db
 
 bcrypt = Bcrypt()
@@ -15,6 +16,8 @@ def create_app(config_class="config.DevelopmentConfig"):
     db.init_app(app)
     bcrypt.init_app(app)
     jwt.init_app(app)
+
+    CORS(app)
 
     from app.api import blueprint
     app.register_blueprint(blueprint)
